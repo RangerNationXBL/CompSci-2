a98726e4cb7d3518c077812ed2d2d0779ff3c6cc
Chris <brown.christopher1978@gmail.com>
2024-01-30 11:35:36 -0800

    added comments

--- Chapter 7 Projects/7-20-8.cpp
+++ Chapter 7 Projects/7-20-8.cpp
@@ -14,18 +14,29 @@ then all students with grade B, and so on.
 #include <vector>
 #include <algorithm>
 
+
+// Create the structure
+
 struct Student{ 
     std::string firstName;
     std::string lastName;
     char grade;
 };
 
+// I am using a pointer here in the arguments of the function.
 void printStudent(const Student* Student){
 
     std::cout << "Student: " << Student->firstName << " " << Student->lastName << std::endl;
     std::cout << "Grade: " << Student->grade << std::endl;
 
 }
+
+// unique_ptr takes responsibility of the pointer (Taking ownership) it is also responsible for deleting the pointer as well
+// I have not fully read up on it so I only have a basic knowledge. 
+// Unique_ptr has two components, a stored pointer and a stored deleter.
+
+// This function adds the pointer to a vector of pointers "move" is used to transfer the object to another. 
+// Many ways to use pointers as I have found.. My preferred is -> "arrow notatation" another is (*pointer).pointed "dot notation"
 
 void addStudent(std::vector<std::unique_ptr<Student> > & students){
     std::unique_ptr<Student> newStudent(new Student());
@@ -34,6 +45,8 @@ void addStudent(std::vector<std::unique_ptr<Student> > & students){
     students.push_back(std::move(newStudent));
 }
 
+// Just creates the desired output for the program.
+// auto can be explicit like "string" probably should be in this case but I have string and char in struct.
 void printStudentByGrade(const std::vector<std::unique_ptr<Student> > & students, char grade){
     for(const auto& student : students){
         if(student->grade == grade){
@@ -43,6 +56,7 @@ void printStudentByGrade(const std::vector<std::unique_ptr<Student> > & students
 
 }
 
+// Making everything work. using a loop to get input and output the data
 int main(){
     std::vector<std::unique_ptr<Student> > students;
 
