1a5cfcd2b0e7330616119c1a9b50dc47e1ded157
Chris <brown.christopher1978@gmail.com>
2024-02-18 11:42:03 -0800

    Just updating the homework

--- Chapter 9/Projects/person/Person.cpp
+++ Chapter 9/Projects/person/Person.cpp
@@ -2,20 +2,40 @@
 
 Person::Person(std::string personName) : name(personName){}
 
-void Person::beFriend(Person p){
-
+/**
+ * Add a friend to the list of friends
+ *
+ * @param p The person to add as a friend
+ * @return Nothing
+ */
+void Person::beFriend(Person p) {
+    if (!isFriend(p)) {
+        if (friendName.empty()) {
+            friendName += " ";
+        }
+        friendName += p.get_name();
+    }
 }
 
 void Person::unFriend(Person p){
-
+    std::size_t pos = friendName.find(p.get_name());
+    if(pos != std::string::npos){
+        friendName.erase(pos, p.get_name().length());
+    }
+    if(pos > 0 && friendName[pos - 1] == ' '){
+        friendName.erase(pos - 1, 1);
+    }
+    if(pos + p.get_name().length() < friendName.length() && friendName[pos + p.get_name().length()] == ' '){
+        friendName.erase(pos + p.get_name().length(), 1);
+    }
 }
 
 std::string Person::get_friend_name(){
-
+    return friendName;
 }
 
 std::string Person::get_name(){
-
+    return name;
 }
 
 bool Person::isFriend(Person p){
